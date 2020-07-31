6cce80749345bafe583ac44fae6e58b97545e8a9
premsn-1226 <65552127+premsn-1226@users.noreply.github.com>
2020-07-31 12:58:13 +0530

    Add files via upload

---
+++ build/precache-manifest.ab6c70faa2f5e2b7a128dc6e7d3635db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99fe62f66eda99a3f5fbbd86a06ced9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047603e49beb300483f7",
+    "url": "/static/css/main.a78c90e0.chunk.css"
+  },
+  {
+    "revision": "1a98a69ecb58d8af4949",
+    "url": "/static/js/2.2c8de82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c8de82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047603e49beb300483f7",
+    "url": "/static/js/main.bed64aa5.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  },
+  {
+    "revision": "9d3ec0e940f3269284d9315236f05767",
+    "url": "/static/media/cloudy.9d3ec0e9.jpg"
+  },
+  {
+    "revision": "e1f0a63384bb0f5dcf3abd3150da392e",
+    "url": "/static/media/winter.e1f0a633.jpg"
+  }
+]);
